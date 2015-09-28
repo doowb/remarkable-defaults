8af4d25c3618c58d1d33965830e70bc884ea6772
Brian Woodward <brian.woodward@gmail.com>
2015-09-28 17:43:01 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * remarkable-defaults <https://github.com/doowb/remarkable-defaults>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
